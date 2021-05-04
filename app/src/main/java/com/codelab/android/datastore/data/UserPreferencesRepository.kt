4d1b45e7fc94de2677b64d3eb33541ab2ba5b467
Rodrigo Miranda <rmiranda@applaudostudios.com>
2021-05-04 17:54:34 -0600

    ✨ Remove sharedPreferences and context from UserPreferencesRepository

--- app/src/main/java/com/codelab/android/datastore/data/UserPreferencesRepository.kt
+++ app/src/main/java/com/codelab/android/datastore/data/UserPreferencesRepository.kt
@@ -16,15 +16,12 @@
 
 package com.codelab.android.datastore.data
 
-import android.content.Context
 import android.util.Log
-import androidx.core.content.edit
 import androidx.datastore.core.DataStore
 import com.codelab.android.datastore.UserPreferences
-import com.codelab.android.datastore.UserPreferences.*
+import com.codelab.android.datastore.UserPreferences.SortOrder
+import com.codelab.android.datastore.UserPreferences.getDefaultInstance
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import java.io.IOException
 
@@ -35,8 +32,7 @@ private const val SORT_ORDER_KEY = "sort_order"
  * Class that handles saving and retrieving user preferences
  */
 class UserPreferencesRepository(
-    private val userPreferencesStore: DataStore<UserPreferences>,
-    context: Context
+    private val userPreferencesStore: DataStore<UserPreferences>
 ) {
 
     private val TAG: String = "UserPreferencesRepo"
@@ -48,22 +44,6 @@ class UserPreferencesRepository(
             } else {
                 throw exception
             }
-        }
-
-    private val sharedPreferences =
-        context.applicationContext.getSharedPreferences(USER_PREFERENCES_NAME, Context.MODE_PRIVATE)
-
-    // Keep the sort order as a stream of changes
-    private val _sortOrderFlow = MutableStateFlow(sortOrder)
-    val sortOrderFlow: StateFlow<SortOrder> = _sortOrderFlow
-
-    /**
-     * Get the sort order. By default, sort order is None.
-     */
-    private val sortOrder: SortOrder
-        get() {
-            val order = sharedPreferences.getString(SORT_ORDER_KEY, SortOrder.NONE.name)
-            return SortOrder.valueOf(order ?: SortOrder.NONE.name)
         }
 
     suspend fun enableSortByDeadline(enable: Boolean) {
@@ -109,12 +89,6 @@ class UserPreferencesRepository(
                     }
                 }
             currentPreferences.toBuilder().setSortOrder(newSortOrder).build()
-        }
-    }
-
-    private fun updateSortOrder(sortOrder: SortOrder) {
-        sharedPreferences.edit {
-            putString(SORT_ORDER_KEY, sortOrder.name)
         }
     }
 
